e6a05b734310db52a38806ecbfe31f246b947da5
gbonillo463 <spamgerard4@gmail.com>
2026-01-28 20:42:10 +0100

    entity class created

--- bills/entity.py
+++ bills/entity.py
@@ -1,26 +1,29 @@
 from abc import ABC, abstractmethod
 
-class Person:
+class Person(ABC):
     def __init__(self, dni: str, email: str, mobile: str):
-        # Write here your code
-        pass
+        self.dni = dni
+        self.email = email
+        self.mobile = mobile
 
     @abstractmethod
     def print(self):
         pass
 
     def __eq__(self, another):
-       # Do not change this method
-       return hasattr(another, 'dni') and self.dni == another.dni
+        # Do not change this method
+        return hasattr(another, 'dni') and self.dni == another.dni
     
     def __hash__(self):
-       # Do not change this method
-       return hash(self.dni)
+        # Do not change this method
+        return hash(self.dni)
 
 class Buyer(Person):
-    def __init__(self, dni: str, email: str, mobile: str, full_name: str, age: int, address: str):
-        # Write here your code
-        pass
+    def __init__(self, dni, email, mobile, full_name: str, age: int, address: str):
+        super().__init__(dni, email, mobile)
+        self.full_name = full_name
+        self.age = age
+        self.adress = address
 
     def print(self):
         # Do not change this method
@@ -28,9 +31,11 @@ class Buyer(Person):
 
 class Seller(Person):
     # Write the parameters in the next line
-    def __init__():
+    def __init__(self, dni, email, mobile, bussines_name: str, bussines_adress: str):
+        super().__init__(dni, email, mobile)
         # Write here your code        
-        pass
+        self.bussines_name = bussines_name
+        self.bussines_adress = bussines_adress
         
     def print(self):
         # Do not change this method
