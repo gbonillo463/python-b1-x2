d7a7a6ca383969fd773d1123091839994020b5ce
gbonillo463 <spamgerard4@gmail.com>
2026-01-30 13:57:46 +0100

    Update stats.py

--- bills/stats.py
+++ bills/stats.py
@@ -20,18 +20,25 @@ class Statistics:
         for bill in self.bills:
             for product in bill.products:
                 products_sells[product] = products_sells.get(product, 0) + 1
+        result = sorted(products_sells.items(), key=lambda x:x[1], reverse=True)
 
-        resultado = sorted(products_sells.items(), key=lambda x:x[1], reverse=True)
-
-        return resultado[0]
+        return result[0]
 
     def find_top_two_sellers(self) -> list:
-        # Write here your code
-        pass
+        sellers = {}
+        for bill in self.bills:
+            sellers[bill.seller] = sellers.get(bill.seller, 0) + bill.calculate_total()
+        result = sorted(sellers.items(), key=lambda x:x[1], reverse=True)
 
+        return [seller[0] for seller in result[:2]]
+    
     def find_buyer_lowest_total_purchases(self) -> (Buyer, float):
-        # Write here your code
-        pass
+        buyers = {}
+        for bill in self.bills:
+            buyers[bill.buyer] = buyers.get(bill.buyer, 0) + bill.calculate_total()
+        result = sorted(buyers.items(), key=lambda x:x[1])
+
+        return result[0]
 
     def order_products_by_tax(self, order_type: OrderType) -> tuple:
         # Write here your code
